707188ea473062dce5abe10d48dafeca4854ed63
Ekaterina Sokolova <105356452+SuperPuperKatushka@users.noreply.github.com>
2025-04-27 20:51:37 +0300

    add start location

--- Long Night/Assets/Scripts/LevelChanger.cs
+++ Long Night/Assets/Scripts/LevelChanger.cs
@@ -4,20 +4,30 @@ using UnityEngine.SceneManagement;
 public class LevelChanger : MonoBehaviour
 {
     private Animator animator;
-    public int levelLoad;
+    public string levelLoad;
+    public Vector3 vector;
 
     private void Start()
     {
         
     }
-    private void FadeToLevel()
-    {
-        animator.SetTrigger("fade"); 
-    }
+    //private void FadeToLevel()
+    //{
+    //    animator.SetTrigger("fade"); 
+    //}
 
-    public void OnFadeComplete()
+    //public void OnFadeComplete()
+    //{
+    //    SceneManager.LoadScene(levelLoad);
+    //}
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        SceneManager.LoadScene(levelLoad);
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelLoad);
+            PlayerPositionData.Instance.savedPosition = vector;
+        }
     }
 }
 
